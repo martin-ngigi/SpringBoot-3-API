db4f672e28c1698670a2dfe1f6ce05e84f4b6ef0
martin-ngigi <martinwainaina001@gmail.com>
2022-12-27 14:16:35 +0300

    Added GET method for hello

--- src/main/java/com/martin/Main.java
+++ src/main/java/com/martin/Main.java
@@ -2,10 +2,19 @@ package com.martin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    //http://localhost:8080/hello
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello, Its Wainaina";
     }
 }
